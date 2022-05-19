a405fca82fe38039dc991b3091706508abec0d93
Esha Khatri <84075946+EshaKhatri@users.noreply.github.com>
2022-05-19 10:18:58 +0530

    Update bubbleSort.c

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+#include<time.h>
 
-int main(){
+void sort(){
     int i,j,n,temp;
     
     printf ("Enter the number of elements in the array : ");
@@ -12,7 +13,7 @@ int main(){
       scanf ("\n%d", &arr[i]);
     }
     
-    printf("Sorting..");
+    printf("\nSorting..");
     
     for(i=0;i<n-1;i++){
         for (j=0;j<n-i-1;j++){
@@ -24,10 +25,18 @@ int main(){
         }
     }
     
-    printf("Sorted array is : ");
+    printf("\nSorted array is : ");
     for (int i = 0; i < n; i++)
     {
       printf("\n%d",arr[i]);
     }
-    return 0;
+}
+
+int main(){
+    clock_t t;
+    t = clock();
+    sort();
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("\nsort() took %f seconds to execute \n", time_taken);
 }
