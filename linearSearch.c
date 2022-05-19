3fb5607371fb4c972955d2b86090fb5e57e1ee7d
Esha Khatri <84075946+EshaKhatri@users.noreply.github.com>
2022-05-19 10:21:47 +0530

    Update linearSearch.c

--- linearSearch.c
+++ linearSearch.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
-
-void
-main ()
-{
-  int n, arr[20], key, y;
+#include<time.h>
+void search(){
+    int n, arr[20], key, y;
   printf ("Enter the number of elements in the array : ");
   scanf ("%d", &n);
 
@@ -27,4 +25,15 @@ main ()
     printf ("\nElement found");
   else
     printf ("\nElement not found");
+}
+
+void
+main ()
+{
+   clock_t t;
+    t = clock();
+    search();
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("\nsearch() took %f seconds to execute \n", time_taken);
 }
