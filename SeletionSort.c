4101916c4c35bc31a9f9110fff097f233b97fa43
Esha Khatri <84075946+EshaKhatri@users.noreply.github.com>
2022-06-09 09:25:49 +0530

    Create SeletionSort.c

---
+++ SeletionSort.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+	int i, j, min_idx;
+
+	for (i = 0; i < n-1; i++)
+	{
+		min_idx = i;
+		for (j = i+1; j < n; j++)
+		if (arr[j] < arr[min_idx])
+			min_idx = j;
+
+		swap(&arr[min_idx], &arr[i]);
+	}
+}
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i=0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+int main()
+{  int n;
+    printf("\nenter the number of elements in the array\n");
+    scanf("  %d",&n);
+     int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+      arr[i] = (random()%1000);
+    }
+    printf("\nArray elements are : ");
+    for (int i = 0; i < n; i++)
+    {
+      printf("\t%d",arr[i]);
+    }
+    clock_t end, start;
+    start = clock();
+	selectionSort(arr, n);
+	end = clock();
+    double time_taken = ((double)(end-start))/CLOCKS_PER_SEC;
+    printf("Sorted array: \n");
+    printf("\nLinear search took %f seconds to execute \n", time_taken);
+	printArray(arr, n);
+	return 0;
+}
